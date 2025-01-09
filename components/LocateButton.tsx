8910cdceebec007071b055345c89bc94e7660f9b
Uli <ulilike161@gmail.com>
2025-01-10 01:06:17 +0200

    log inaccuracy of geolocation (I'll probably need to switch to Google API or something else external)

--- components/LocateButton.tsx
+++ components/LocateButton.tsx
@@ -11,25 +11,38 @@ export default function LocateButton({ onLocationFound }: LocateButtonProps) {
   const map = useMap();
 
   function getUserCoords(position: any) {
-    const { latitude, longitude } = position.coords;
+    const { latitude, longitude, accuracy } = position.coords;
     const location: [number, number] = [latitude, longitude];
+    console.log(`Accuracy: ${accuracy} meters`);
+    if (accuracy > 100) {
+      alert(
+        "Ваша ґеолокація може бути неточною. Точність: " + accuracy + " метрів."
+      );
+    }
     onLocationFound(location);
     map.flyTo(location, 16);
   }
 
   function locateUser() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(getUserCoords, (error) => {
-        console.error("Помилка отримання місцезнаходження: ", error);
-        alert(
-          "Не вдалось отримати Ваше місцезнаходження. Будь ласка, спробуйте ще раз."
-        );
-      });
+      navigator.geolocation.getCurrentPosition(
+        getUserCoords,
+        (error) => {
+          console.error("Помилка отримання місцезнаходження: ", error);
+          alert(
+            "Не вдалось отримати Ваше місцезнаходження. Будь ласка, спробуйте ще раз."
+          );
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 10000,
+          maximumAge: 0,
+        }
+      );
     } else {
       alert("Ґеолокація не підтримується Вашим браузером.");
     }
   }
-
   return (
     <button className={styles.locateButton} onClick={locateUser}>
       Де я?
