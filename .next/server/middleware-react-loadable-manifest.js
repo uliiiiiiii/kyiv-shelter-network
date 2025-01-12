0fc426f178b023b6b2f7af1822e9aeb91906bad3
Uli <ulilike161@gmail.com>
2025-01-12 17:46:32 +0200

    everything works as it intended (but it doesn't build)

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app/page.tsx -> @/components/Map\":{\"id\":\"app/page.tsx -> @/components/Map\",\"files\":[\"static/css/_app-pages-browser_components_Map_tsx.css\",\"static/chunks/_app-pages-browser_components_Map_tsx.js\"]}}"
+self.__REACT_LOADABLE_MANIFEST="{}"
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 
+import Map from "@/components/Map";
+
 import dynamic from "next/dynamic";
 import styles from "@/styles/Home.module.css";
 
-const Map = dynamic(() => import("@/components/Map"), {
-  ssr: false,
-  loading: () => <p>Loading map...</p>,
-});
+// const Map = dynamic(() => import("@/components/Map"), {
+//   ssr: false,
+//   loading: () => <p>Loading map...</p>,
+// });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Map />
-    </main>
+    // <main className={styles.main}>
+    <Map />
+    // </main>
   );
 }
