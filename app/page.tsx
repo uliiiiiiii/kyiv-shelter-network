a1ee5d47fbee3b0e7e3516a4317a2d2b9e66ad13
Uli <ulilike161@gmail.com>
2025-01-31 21:36:19 +0200

    show/hide panel with filters

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import dynamic from "next/dynamic";
+import Map from "@/components/Map";
 
-const Map = dynamic(() => import("@/components/Map"), {
-  ssr: false,
-  loading: () => <p>Loading map...</p>,
-});
+// const Map = dynamic(() => import("@/components/Map"), {
+//   ssr: false,
+//   loading: () => <p>Loading map...</p>,
+// });
 
 export default function Home() {
   return (
