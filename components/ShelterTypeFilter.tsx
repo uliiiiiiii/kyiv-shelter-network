a8d38ae992d8e02d45978689f316a9cc0cf8c79e
Uli <ulilike161@gmail.com>
2025-02-02 23:57:53 +0200

    updated responsive styles

--- components/ShelterTypeFilter.tsx
+++ components/ShelterTypeFilter.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import getMarkerColor from "@/utils/getMarkerColor";
 import styles from "@/styles/Map.module.css";
 
@@ -13,43 +13,55 @@ export default function ShelterTypeFilter({
   selectedTypes,
   onTypeChange,
 }: ShelterTypeFilterProps) {
+  const [isVisible, setVisible] = useState(false);
   return (
     <div className={styles.filterPanel}>
-      <h3 style={{ paddingBottom: "10px", paddingRight: "30px" }}>
-        Фільтрувати укриття:
-      </h3>
-      {types.map((type) => {
-        const isChecked = selectedTypes.includes(type);
-        return (
-          <label
-            key={type}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              marginBottom: "8px",
-              cursor: "pointer",
-            }}
-          >
-            <input
-              type="checkbox"
-              value={type}
-              checked={isChecked}
-              onChange={() => onTypeChange(type)}
-              style={{ display: "none" }}
-            />
-            <div
-              className={styles.checkbox}
+      <div style={{ display: isVisible ? "block" : "none" }}>
+        <h3 style={{ paddingBottom: "10px", paddingRight: "30px" }}>
+          Фільтрувати укриття:
+        </h3>
+        {types.map((type) => {
+          const isChecked = selectedTypes.includes(type);
+          return (
+            <label
+              key={type}
               style={{
-                backgroundColor: getMarkerColor(type),
-                border: `2px solid ${isChecked ? "black" : "white"}`,
+                display: "flex",
+                alignItems: "center",
+                marginBottom: "8px",
+                cursor: "pointer",
               }}
             >
-              {isChecked && <div className={styles.checkedInput} />}
-            </div>
-            {type}
-          </label>
-        );
-      })}
+              <input
+                type="checkbox"
+                value={type}
+                checked={isChecked}
+                onChange={() => onTypeChange(type)}
+                style={{ display: "none" }}
+              />
+              <div
+                className={styles.checkbox}
+                style={{
+                  backgroundColor: getMarkerColor(type),
+                  border: `2px solid ${isChecked ? "black" : "white"}`,
+                }}
+              >
+                {isChecked && <div className={styles.checkedInput} />}
+              </div>
+              {type}
+            </label>
+          );
+        })}
+      </div>
+
+      <button
+        className={`${styles.showFiltersButton} ${
+          isVisible ? styles.openedFiltersButton : ""
+        }`}
+        onClick={() => setVisible(!isVisible)}
+      >
+        {isVisible ? "Приховати фільтри" : "Фільтри"}
+      </button>
     </div>
   );
 }
